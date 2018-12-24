cf4481ccbc09736d256da20faae60c39ed30525f
Davi-Schumacher <daschumacher@dons.usfca.edu>
2018-12-23 21:03:20 -0800

    change function inputs to allow for different column names

--- ks_2samp_sparksql.py
+++ ks_2samp_sparksql.py
@@ -9,40 +9,40 @@ FILLED_CDF_2 = 'filled_cdf_2'
 
 def get_cdf(df, variable, col_name):
 
-	cdf = df.select(variable).na.drop().\
-		withColumn(
-			col_name,
-			funcs.cume_dist().over(Window.orderBy(variable))
-		).distinct()
+    cdf = df.select(variable).na.drop().\
+        withColumn(
+            col_name,
+            funcs.cume_dist().over(Window.orderBy(variable))
+        ).distinct()
 
-	return cdf
+    return cdf
 
 
-def ks_2samp(df1, df2, variable):
+def ks_2samp(df1, var1, df2, var2):
 
-	ks_stat = get_cdf(df1, variable, CDF_1).\
-		join(
-			get_cdf(df2, variable, CDF_2),
-			on=variable,
-			how='outer'
-		).\
-		withColumn(
-			FILLED_CDF_1,
-			funcs.last(funcs.col(CDF_1), ignorenulls=True).
-			over(Window.rowsBetween(Window.unboundedPreceding, Window.currentRow))
-		).\
-		withColumn(
-			FILLED_CDF_2,
-			funcs.last(funcs.col(CDF_2), ignorenulls=True).
-			over(Window.rowsBetween(Window.unboundedPreceding, Window.currentRow))
-		).\
-		select(
-			funcs.max(
-				funcs.abs(
-					funcs.col(FILLED_CDF_1) - funcs.col(FILLED_CDF_2)
-				)
-			)
-		).\
-		collect()[0][0]
+    ks_stat = get_cdf(df1, var1, CDF_1).\
+        join(
+            get_cdf(df2, var2, CDF_2),
+            on=var1 == var2,
+            how='outer'
+        ).\
+        withColumn(
+            FILLED_CDF_1,
+            funcs.last(funcs.col(CDF_1), ignorenulls=True).
+            over(Window.rowsBetween(Window.unboundedPreceding, Window.currentRow))
+        ).\
+        withColumn(
+            FILLED_CDF_2,
+            funcs.last(funcs.col(CDF_2), ignorenulls=True).
+            over(Window.rowsBetween(Window.unboundedPreceding, Window.currentRow))
+        ).\
+        select(
+            funcs.max(
+                funcs.abs(
+                    funcs.col(FILLED_CDF_1) - funcs.col(FILLED_CDF_2)
+                )
+            )
+        ).\
+        collect()[0][0]
 
-	return ks_stat
+    return ks_stat
